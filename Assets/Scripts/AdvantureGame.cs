67daf955acf7ccaea6fd9dd795f51610544b1437
Anil Yanik <anilyanik98@gmail.com>
2021-01-16 17:31:52 +0100

    second commit

---
+++ Assets/Scripts/AdvantureGame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdvantureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] state startingState;
+
+    state state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1)) 
+        {
+            state = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[2];
+        }
+        textComponent.text = state.GetStateStory();
+    }
+}
---
+++ Assets/Scripts/state.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "state")]
+
+public class state : ScriptableObject
+{
+    [TextArea(14,10)][SerializeField] string StoryText;
+    [SerializeField] state[] nextStates;
+    public string GetStateStory()
+    {
+        return StoryText;
+
+    }
+
+    public state[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
